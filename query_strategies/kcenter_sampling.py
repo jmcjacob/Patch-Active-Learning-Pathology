099d36eef4b2e249ae359b9f685f00c0904d0a61
Jacob Carse <jacob.carse@gmail.com>
2019-01-18 22:17:28 +0000

    Update kcenter_sampling.py

--- query_strategies/kcenter_sampling.py
+++ query_strategies/kcenter_sampling.py
@@ -10,37 +10,6 @@ class KCentreGreedySampling(Strategy):
     calculated using the greedy method. The centres of the covers are selected to be annotated.
     """
 
-    # def query(self, n):
-    #     """
-    #     Method for querying the data to be labeled. This method selects data that are centers of clusters specified by
-    #     the K-Centre cover method.
-    #     :param n: Amount of data to query.
-    #     :return: Array of indices to data selected to be labeled.
-    #     """
-    #
-    #     labeled_flag = self.labeled_indices.copy()
-    #     embedding = self.get_embeddings(self.x, self.y)
-    #     embedding = embedding.numpy()
-    #
-    #     distance_matrix = np.dot(embedding.astype(np.float16), embedding.transpose().astype(np.float16))
-    #     square = np.array(distance_matrix.diagonal()).reshape(len(self.x), 1)
-    #     distance_matrix *= -2
-    #     distance_matrix += square
-    #     distance_matrix += square.transpose()
-    #     distance_matrix = np.sqrt(distance_matrix)
-    #
-    #     matrix = distance_matrix[~labeled_flag, :][:, labeled_flag]
-    #
-    #     for i in range(n):
-    #         matrix_minimum = matrix.min(axis=1)
-    #         q_index_ = matrix_minimum.argmax()
-    #         q_index = np.arange(self.pool_size)[~labeled_flag][q_index_]
-    #         labeled_flag[q_index] = True
-    #         matrix = np.delete(matrix, q_index_, 0)
-    #         matrix = np.append(matrix, distance_matrix[~labeled_flag, q_index][:, None], axis=1)
-    #
-    #     return np.arange(self.pool_size)[(self.labeled_indices ^ labeled_flag)]
-
     def query(self, n):
         unlabeled_indices = np.arange(self.pool_size)[~self.labeled_indices]
         labeled_indices = np.arange(self.pool_size)[self.labeled_indices]
